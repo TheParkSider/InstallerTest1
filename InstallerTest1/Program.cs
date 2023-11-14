b184c5350f38e19164c1bc6c564ee45bb70ded2a
TheParkSider <theparksider@yahoo.co.jp>
2023-11-14 22:30:25 +0900

    プロジェクト ファイルを追加します。

---
+++ InstallerTest1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace InstallerTest1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Installer Test");
+            Console.ReadKey();
+        }
+    }
+}
